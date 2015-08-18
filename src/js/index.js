e4a56311a75852d3d71f9db27ff3f23b08fe75f5
raingxm <raingxm@163.com>
2015-08-18 21:53:57 +0800

    fetch all city

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,18 @@
     };
 
     exports.fetchCity = function(cityName) {
+        $.get(exports.getServerUrl(cityName), function(data) {
+            _.each(data, exports.renderCity);
+        });
+    }
 
+    exports.renderCity = function(eachCity) {
+        var template = _.template('<div id="results"><div class="panel large-12 columns">' +
+            '<h5><%= name %></h5><h6><%= description %></h6>' +
+            '<a href="#" class="like button tiny right">Like</a></div></div>');
+        var content = template({name: eachCity.name,
+                        description: eachCity.description});
+        $('#searchResults').append($(content));
     }
 
     exports.getServerUrl = function(cityName) {
